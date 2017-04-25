3a12b6df88b4df7073eeecf7b6ea4b436b571450
Chris <dcbrown06@gmail.com>
2017-04-25 15:38:12 -0400

    Adding error prone example error

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,13 @@
+import java.util.*;
+import java.io.*;
+
 public class HelloWorld {
     public static void main(String[] args) {
-           System.out.println("Hello Jenkins");
+        Set<Short> s = new HashSet<>();
+        for (short i = 0; i < 100; i++) {
+          s.add(i);
+          s.remove(i - 1);
+        }
+        System.out.println(s.size());
     }
 }
