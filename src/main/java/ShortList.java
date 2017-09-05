637f3f8f7dbc9304f4a8b281ae44af32f81afcec
Chris <dcbrown06@gmail.com>
2017-09-05 16:32:20 -0400

    Adding similar error in new file

---
+++ src/main/java/ShortList.java
@@ -0,0 +1,12 @@
+public class ShortList {
+  public static void main (String[] args) {
+    List<Short> s = new ArrayList<>();
+    for (short i = 0; i < 100; i++) {
+      s.add(i);
+      s.remove(i - 1);
+    }
+    System.out.println(s.size());
+  }
+}
+
+
