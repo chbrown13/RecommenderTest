cee6aa2eab1f13023292e80b90489f58d06dd043
Chris <dcbrown06@gmail.com>
2017-09-06 09:55:54 -0400

    Shorten ShortList

--- src/main/java/ShortList.java
+++ src/main/java/ShortList.java
@@ -1,7 +1,7 @@
 public class ShortList {
   public static void main (String[] args) {
     List<Short> s = new ArrayList<>();
-    for (short i = 0; i < 100; i++) {
+    for (short i = 0; i < 10; i++) {
       s.add(i);
       s.remove(i - 1);
     }
