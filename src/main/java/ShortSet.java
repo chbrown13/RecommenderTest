68ad591ff161de374425612d044d2f398f12e2f3
Chris <dcbrown06@gmail.com>
2017-09-25 10:02:10 -0400

    Removing error prone line

--- src/main/java/ShortSet.java
+++ src/main/java/ShortSet.java
@@ -5,7 +5,6 @@ public class ShortSet {
     Set<Short> s = new HashSet<>();
     for (short i = 0; i < 100; i++) {
       s.add(i);
-      s.remove(i - 1);
     }
     System.out.println(s.size());
   }
