05f655bbf3bfd8738007c2af426c001279cc8b56
Chris <dcbrow10@ncsu.edu>
2017-09-25 10:00:30 -0400

    Improving error for testing

--- src/main/java/LongSet.java
+++ src/main/java/LongSet.java
@@ -1,8 +1,11 @@
+import java.util.*;
+
 public class LongSet {
   public static void main (String[] args) {
     Set<Short> s = new HashSet<>();
     for (short i = 0; i < 1000; i++) {
       s.add(i);
+	  s.remove(i - 1);
     }
     System.out.println(s.size());
   }
--- src/main/java/ShortList.java
+++ src/main/java/ShortList.java
@@ -1,8 +1,9 @@
+import java.util.*;
+
 public class ShortList {
   public static void main (String[] args) {
     List<Short> s = new ArrayList<>();
     for (short i = 0; i < 100; i++) {
-      s.add(i);
       s.remove(i - 1);
     }
     System.out.println(s.size());
