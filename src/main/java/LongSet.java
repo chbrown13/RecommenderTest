a82edb754675434ce8c6137cf431b0498c1345b6
Chris <dcbrown06@gmail.com>
2017-09-26 11:55:36 -0400

    Fix CollectionIncompatibleType
    Jenkins timer test

--- src/main/java/LongSet.java
+++ src/main/java/LongSet.java
@@ -4,8 +4,9 @@ public class LongSet {
   public static void main (String[] args) {
     Set<Short> s = new HashSet<>();
     for (short i = 0; i < 1000; i++) {
+      short sh = i - 1;
       s.add(i);
-	  s.remove(i - 1);
+      s.remove(sh);
     }
     System.out.println(s.size());
   }
