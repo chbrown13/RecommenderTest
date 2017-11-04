b168ff1a4e4c14204b9aa54517e4a76a51620184
Chris <dcbrown06@gmail.com>
2017-11-04 09:58:01 -0400

    Fix Short error
    change to ints

--- src/main/java/LongSet.java
+++ src/main/java/LongSet.java
@@ -2,10 +2,10 @@ import java.util.*;
 
 public class LongSet {
   public static void main (String[] args) {
-    Set<Short> s = new HashSet<Short>();
-    for (short i = 0; i < 1000; i++) {
-      s.add(i);
-	  s.remove(i - 1);
+    Set<Integer> s = new HashSet<Integer>();
+    for (int i = 0; i < 1000; i++) {
+        s.add(i);
+	s.remove(i - 1);
     }
     System.out.println(s.size());
   }
