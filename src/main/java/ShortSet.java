99f9b90c54cfd3dd6dbbf6ce1edf33409c23671c
Chris <dcbrown06@gmail.com>
2017-11-29 15:32:43 -0500

    code change

--- src/main/java/ShortSet.java
+++ src/main/java/ShortSet.java
@@ -3,9 +3,9 @@ import java.util.*;
 public class ShortSet {
   public static void main (String[] args) {
     Set<Short> s = new HashSet<Short>();
-    for (short i = 0; i < 100; i++) {
-      s.add(i);
-      s.remove(i - 1);
+    for (short it = 0; it < 100; it++) {
+      s.add(it);
+      s.remove(it - 1);
     }
     System.out.println(s.size());
   }
