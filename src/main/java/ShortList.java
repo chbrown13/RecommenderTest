763a3dad94f75fbb71524e93e4e423105ebb6e8f
Chris <dcbrown06@gmail.com>
2017-11-29 15:34:32 -0500

    Actual fix change

--- src/main/java/ShortList.java
+++ src/main/java/ShortList.java
@@ -4,8 +4,8 @@ public class ShortList {
   public static void main (String[] args) {
     Set<Short> s = new HashSet<Short>();
     for (short i = 0; i < 100; i++) {
-      s.add(i);
-      s.remove(i - 1);
+      Short sh = i - 1;
+      s.remove(sh);
     }
     System.out.println(s.size());
   }
