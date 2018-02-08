986d9d942a40ae818de1de764ac566bb2f8f96c6
Chris <dcbrown06@gmail.com>
2018-02-08 10:49:46 -0500

    VCL fix test
    send email and run mvn

--- src/main/java/ShortSet.java
+++ src/main/java/ShortSet.java
@@ -4,8 +4,9 @@ public class ShortSet {
   public static void main (String[] args) {
     Set<Short> s = new HashSet<Short>();
     for (short i = 0; i < 100; i++) {
+      short sho = i - 1;
       s.add(i);
-      s.remove(i - 1);
+      s.remove(sho);
     }
     System.out.println(s.size());
   }
