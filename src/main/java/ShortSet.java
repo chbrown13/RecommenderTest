4c2c3ec0a00f92083533cf72eccc90ca742a3939
Chris <dcbrown06@gmail.com>
2018-02-09 10:51:37 -0500

    Add error back

--- src/main/java/ShortSet.java
+++ src/main/java/ShortSet.java
@@ -4,9 +4,8 @@ public class ShortSet {
   public static void main (String[] args) {
     Set<Short> s = new HashSet<Short>();
     for (short i = 0; i < 100; i++) {
-      short a = i - 1;
       s.add(i);
-      s.remove(a);
+      s.remove(i - 1);
     }
     System.out.println(s.size());
   }
