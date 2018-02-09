a1c886adee565d36cb1883709954fa6c8265db04
Chris <dcbrown06@gmail.com>
2018-02-09 11:58:02 -0500

    test
    line numbers

--- src/main/java/ShortSet.java
+++ src/main/java/ShortSet.java
@@ -2,10 +2,12 @@ import java.util.*;
 
 public class ShortSet {
   public static void main (String[] args) {
+    int x = 0;
     Set<Short> s = new HashSet<Short>();
     for (short i = 0; i < 100; i++) {
+      short sh = i - 1;
       s.add(i);
-      s.remove(i - 1);
+      s.remove(sh);
     }
     System.out.println(s.size());
   }
