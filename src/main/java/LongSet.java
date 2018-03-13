79b9532c45924e57bfe191a1c2ecd739727e865b
Chris Brown <dcbrown06@gmail.com>
2018-03-13 18:56:41 -0400

    java change
    test

--- src/main/java/LongSet.java
+++ src/main/java/LongSet.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class LongSet {
   public static void main (String[] args) {
     Set<Short> s = new HashSet<Short>();
-    for (short i = 0; i < 1000; i++) {
+    for (short i = 0; i < 100000; i++) {
       s.add(i);
 	  s.remove(i - 1);
     }
