4d0de07614bba2b05c141638ae9113e824e1e6b3
Chris Brown <dcbrown06@gmail.com>
2018-10-26 13:56:05 -0400

    Update src/main/java/ShortSet.java

--- src/main/java/ShortSet.java
+++ src/main/java/ShortSet.java
@@ -5,7 +5,7 @@ public class ShortSet {
     Set<Short> s = new HashSet<Short>();
     for (short i = 0; i < 100; i++) {
       short sh = i - 1;
-      s.add(i);
+      s.remove(i);
       s.remove(sh);
     }
     System.out.println(s.size());
